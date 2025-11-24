d037120b5e3df622b6ea1a02f1c029974cca7d55
ryanvo504 <ryanvo@ucsb.edu>
2025-11-23 20:14:58 -0800

    new map

--- client/src/App.js
+++ client/src/App.js
@@ -511,8 +511,8 @@ function App() {
             searchedLocation={searchedLocation}
           />
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png`}
           />
 
           {/* Route polyline */}
